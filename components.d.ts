428a20e4745b1bb00c5161e11543dba4c0b9743a
zjy2414 <zjy2414@outlook.com>
2023-10-29 19:41:23 +0800

    update

--- components.d.ts
+++ components.d.ts
@@ -7,9 +7,17 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElProgress: typeof import('element-plus/es')['ElProgress']
+    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
+    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
+    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
+    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
+    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
+    NavBar: typeof import('./src/components/NavBar.vue')['default']
+    PageFooter: typeof import('./src/components/PageFooter.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    VideoCard: typeof import('./src/components/VideoCard.vue')['default']
+    VideoPlayer: typeof import('./src/components/VideoPlayer.vue')['default']
   }
 }
