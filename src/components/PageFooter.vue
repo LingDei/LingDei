e5d6554b27bc3236989631fbc85d1b0917bbd24a
zjy2414 <zjy2414@outlook.com>
2023-10-29 23:12:00 +0800

    update: 修改底部

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -4,8 +4,8 @@
 
 <template>
     <div class="p-4 mt-8 text-white bg-gray-800">
-        <div class="container mx-auto text-center">Copyright &copy; 2023 灵嘚（LingDei）咚易科技 | 沪ICP备14039016号-17
- </div>
+        <div class="container mx-auto text-center">Copyright &copy; 2023 灵嘚（LingDei）版权所有</div>
+        <div class="container mx-auto text-center">咚易科技 | 沪ICP备14039016号-17</div>
     </div>
 </template>
 
