fbcbb99bd77b0ef0c7e2c527447abbc87a05fd98
zjy2414 <zjy2414@outlook.com>
2023-10-29 17:20:26 +0800

    update: 增加视频卡片组件及其示例

---
+++ src/components/VideoCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Video } from '@/model/video';
+
+defineProps({
+    video: {
+        type: Object as () => Video,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div class="overflow-hidden bg-white rounded-lg shadow-md">
+        <img :src="video.cover" :alt="video.title" class="object-cover w-full h-48" />
+        <div class="p-4">
+            <h2 class="text-xl font-semibold">{{ video.title }}</h2>
+            <p class="text-gray-600">{{ video.description }}</p>
+            <div class="mt-4">
+                <span class="text-gray-500">观看次数: {{ video.views }}</span>
+                <span class="ml-4 text-gray-500">发布日期: {{ video.date }}</span>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/model/video.ts
@@ -0,0 +1,12 @@
+interface Video {
+    id: string;
+    cover: string;
+    title: string;
+    description: string;
+    views: string;
+    date: string;
+}
+
+export type {
+    Video
+}
