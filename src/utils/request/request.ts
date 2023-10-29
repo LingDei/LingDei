6a83a39be610776f388c6bfe9d137c6e54a17ccb
风夏小雪 <493067153@qq.com>
2023-10-29 19:44:22 +0800

    feat: videoList实现

--- src/utils/request/request.ts
+++ src/utils/request/request.ts
@@ -1,20 +1,10 @@
 import axios from 'axios'
 
-import {
-  handleNetworkError
-} from './RequestTools'
-import { baseURL } from '@/constants/server';
-
-type Fn = (data: FcResponse<any>) => unknown;
+import { handleNetworkError } from './RequestTools'
+import { baseURL } from '@/constants/server'
 
 interface IAnyObj {
-  [index: string]: unknown;
-}
-
-export interface FcResponse<T> {
-  errno: string;
-  errmsg: string;
-  data: T;
+  [index: string]: unknown
 }
 
 const request = axios.create({
@@ -25,55 +15,46 @@ const request = axios.create({
 
 request.interceptors.request.use((config) => {
   // 此处可以添加请求头相关信息
-  return config;
-});
+  return config
+})
 
 request.interceptors.response.use(
   (response) => {
-    if (response.status !== 200) return Promise.reject(response.data);
-    return response;
+    if (response.status !== 200) return Promise.reject(response.data)
+    return response
   },
   (err) => {
-    handleNetworkError(err.response.status);
-    Promise.reject(err.response);
+    handleNetworkError(err.response.status)
+    Promise.reject(err.response)
   }
-);
+)
 
-export const Get = <T,>(
-  url: string,
-  params: IAnyObj = {},
-  clearFn?: Fn
-): Promise<[any, FcResponse<T> | undefined]> =>
+export const Get = <T>(url: string, params: IAnyObj = {}): Promise<[any, T | undefined]> =>
   new Promise((resolve) => {
     request
       .get(url, { params })
       .then((result) => {
-        let res: FcResponse<T>;
-        if (clearFn !== undefined) {
-          res = clearFn(result.data) as unknown as FcResponse<T>;
-        } else {
-          res = result.data as FcResponse<T>;
-        }
-        resolve([null, res as FcResponse<T>]);
+        const res: T = result.data
+        resolve([null, res])
       })
       .catch((err) => {
-        resolve([err, undefined]);
-      });
-  });
+        resolve([err, undefined])
+      })
+  })
 
-export const Post = <T,>(
+export const Post = <T>(
   url: string,
   data: IAnyObj,
   params: IAnyObj = {}
-): Promise<[any, FcResponse<T> | undefined]> => {
+): Promise<[any, T | undefined]> => {
   return new Promise((resolve) => {
     request
       .post(url, data, { params })
       .then((result) => {
-        resolve([null, result.data as FcResponse<T>]);
+        resolve([null, result.data])
       })
       .catch((err) => {
-        resolve([err, undefined]);
-      });
-  });
-};
+        resolve([err, undefined])
+      })
+  })
+}
