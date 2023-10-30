ce40f5a89923215a146d9365f26fdf1aa5075a6a
zjy2414 <zjy2414@outlook.com>
2023-10-30 12:00:38 +0800

    update: 增加auth请求体

---
+++ src/utils/request/auth_request.ts
@@ -0,0 +1,60 @@
+import axios from 'axios'
+
+import { handleNetworkError } from './RequestTools'
+import { authURL } from '@/constants/server'
+
+interface IAnyObj {
+  [index: string]: unknown
+}
+
+const request = axios.create({
+  baseURL: authURL,
+  timeout: 10000,
+  responseType: 'json'
+})
+
+request.interceptors.request.use((config) => {
+  // 此处可以添加请求头相关信息
+  return config
+})
+
+request.interceptors.response.use(
+  (response) => {
+    if (response.status !== 200) return Promise.reject(response.data)
+    return response
+  },
+  (err) => {
+    handleNetworkError(err.response.status)
+    Promise.reject(err.response)
+  }
+)
+
+export const Get = <T>(url: string, params: IAnyObj = {}): Promise<[any, T | undefined]> =>
+  new Promise((resolve) => {
+    request
+      .get(url, { params })
+      .then((result) => {
+        const res: T = result.data
+        resolve([null, res])
+      })
+      .catch((err) => {
+        resolve([err, undefined])
+      })
+  })
+
+export const Post = <T>(
+  url: string,
+  data: IAnyObj,
+  params: IAnyObj = {}
+): Promise<[any, T | undefined]> => {
+  return new Promise((resolve) => {
+    request
+      .post(url, data, { params })
+      .then((result) => {
+        resolve([null, result.data])
+      })
+      .catch((err) => {
+        resolve([err, undefined])
+      })
+  })
+}
