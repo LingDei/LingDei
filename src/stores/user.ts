84e530bd3242739d3bc23c6ede0c39dc2090d9fb
zjy2414 <zjy2414@outlook.com>
2023-10-30 23:59:35 +0800

    update: setToken时查询用户信息一并持久化存储

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,11 +1,27 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import type { Profile } from '@/model/profile'
+import { apis } from '@/apis'
+import { handleNetworkError } from '@/utils/request/RequestTools'
 
 export const useUserStore = defineStore('user', () => {
   const token = ref('')
+  const profile: Ref<Profile> = ref({
+    id: '',
+    nickname: '',
+    email: '',
+    avatar_url: ''
+  })
 
-  function setToken(newToken: string) {
+
+  async function setToken(newToken: string) {
     token.value = newToken
+
+    // 获取用户信息
+    const [err, data] = await apis.getProfile()
+    if (err) handleNetworkError(err)
+    if (!data) return
+    profile.value = data.profile
   }
 
   const username = computed(() => {
