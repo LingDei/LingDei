1c157d576efcb8ba16f84dbdadd268471f5523a9
zjy2414 <zjy2414@outlook.com>
2023-10-31 23:00:12 +0800

    update: 支持多布局

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import LNavBar from '@/components/NavBar.vue'
+import LFooter from '@/components/PageFooter.vue';
 </script>
 
 <template>
+  <LNavBar />
+
   <RouterView />
+
+  <LFooter />
 </template>
 
 <style scoped>
