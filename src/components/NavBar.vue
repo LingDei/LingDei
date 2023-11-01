18be527caac7a7d6b1846a7fa27bd9661af01dc6
zjy2414 <zjy2414@outlook.com>
2023-11-01 10:45:44 +0800

    update: 优化机制

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,13 +4,30 @@ import { apis } from '@/apis'
 import type { Category } from '@/model/category';
 import { handleNetworkError } from '@/utils/request/RequestTools';
 
-const categories = ref<Category[]>([])
+interface Tab {
+    type: string // default or category
+    name: string
+    path: string
+}
+
+const tabs = ref<Tab[]>([
+    { type: 'default', name: '首页', path: '/' },
+    { type: 'default', name: '推荐', path: '/recommend' },
+    { type: 'default', name: '关注', path: '/follow' },
+])
 
 onMounted(async () => {
     const [err, data] = await apis.getCategoryList()
     if (err) handleNetworkError(err)
     if (!data || data?.category_list.length === 0) return
-    categories.value = data.category_list
+    // 将分类加入tabs
+    data.category_list.forEach((category: Category) => {
+        tabs.value.push({
+            type: 'category',
+            name: category.name,
+            path: `/category?uuid=${category.uuid}`,
+        })
+    })
 })
 
 </script>
@@ -19,10 +36,8 @@ onMounted(async () => {
     <!-- 导航栏 -->
     <nav class="p-4 text-white bg-gradient-to-r from-blue-900 to-blue-800">
         <div class="container flex items-center justify-between mx-auto">
-            <!-- 引用assets下的logo.svg -->
+            <!-- Logo -->
             <img src="../assets/logo-white.svg" alt="logo" class="w-24 h-15" />
-
-            <!-- <a href="#" class="text-3xl font-bold">灵嘚</a> -->
 
             <div class="flex items-center">
                 <!-- 用户头像 -->
@@ -32,15 +47,14 @@ onMounted(async () => {
 
         <div class="container mx-auto mt-4">
             <div class="flex space-x-4">
-                <RouterLink to="/" class="text-gray-300 hover:text-white">首页</RouterLink>
-                <RouterLink to="/recommend" class="text-gray-300 hover:text-white">推荐</RouterLink>
-                <RouterLink to="/follow" class="text-gray-300 hover:text-white">关注</RouterLink>
-                <RouterLink v-for="category in categories" :key="category.uuid" :to="`/category?uuid=${category.uuid}`"
-                    class="text-gray-300 hover:text-white">{{ category.name }}</RouterLink>
+                <RouterLink v-for="tab in tabs" :key="tab.tag" :to="tab.path" class="text-gray-300 hover:text-white">{{
+                    tab.name }}</RouterLink>
             </div>
         </div>
     </nav>
 </template>
-  
+
+<style>
+</style>
 
   
