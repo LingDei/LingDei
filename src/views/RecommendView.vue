78ed6104aaab1e8c7c5abb8aa7e71cf5d8cb3a91
zjy2414 <zjy2414@outlook.com>
2023-11-01 18:25:23 +0800

    fix

--- src/views/RecommendView.vue
+++ src/views/RecommendView.vue
@@ -1,35 +1,35 @@
 <script setup lang="ts">
-  import VideoPlayer from '@/components/VideoPlayer.vue'
-  import { onMounted, ref, computed } from 'vue'
-  // @ts-ignore TODO: 配置了include还是无法解析
-  import { apis } from '@/apis'
-  import { handleNetworkError } from '@/utils/request/RequestTools'
-  import type { Video } from '@/model/video'
+import VideoPlayer from '@/components/VideoPlayer.vue'
+import { onMounted, ref, computed } from 'vue'
+// @ts-ignore TODO: 配置了include还是无法解析
+import { apis } from '@/apis'
+import { handleNetworkError } from '@/utils/request/RequestTools'
+import type { Video } from '@/model/video'
 
-  const videoList = ref < Video[] > ([])
-  const index = ref(0)
+const videoList = ref<Video[]>([])
+const index = ref(0)
 
-  onMounted(async () => {
-    const [err, data] = await apis.getVideoList()
-    if (err) handleNetworkError(err)
-    if (!data || data?.video_list.length === 0) return
-    videoList.value = data.video_list
-  })
+onMounted(async () => {
+  const [err, data] = await apis.getVideoList()
+  if (err) handleNetworkError(err)
+  if (!data || data?.video_list.length === 0) return
+  videoList.value = data.video_list
+})
 
-  const indexInfo = computed(() => ({
-    index: index.value,
-    indexs: [index.value - 1, index.value, index.value + 1]
-  }))
+const indexInfo = computed(() => ({
+  index: index.value,
+  indexs: [index.value - 1, index.value, index.value + 1]
+}))
 
-  function up() {
-    if (index === 0) return
-    index.value--
-  }
+function up() {
+  if (index.value === 0) return
+  index.value--
+}
 
-  function down() {
-    if(index === videoList.length) return
-    index.value++
-  }
+function down() {
+  if (index.value === videoList.value.length) return
+  index.value++
+}
 
 </script>
 
@@ -54,18 +54,18 @@
 </template>
 
 <style lang="less">
-  .videoList {
-    margin: 0 auto;
-    width: 60%;
-    height: 90%;
-    min-height: 400px;
-  }
+.videoList {
+  margin: 0 auto;
+  width: 60%;
+  height: 90%;
+  min-height: 400px;
+}
 
-  .switchVideo {
-    display: flex;
-    flex-direction: column;
-    border-radius: 30%;
-    width: 60px;
-    height: 120px;
-  }
+.switchVideo {
+  display: flex;
+  flex-direction: column;
+  border-radius: 30%;
+  width: 60px;
+  height: 120px;
+}
 </style>
