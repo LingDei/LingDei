ba3bf2afaeed39603423a6ab05c3c32ef4779f1f
zjy2414 <zjy2414@outlook.com>
2023-11-01 12:32:02 +0800

    update: 更新接口调用方式

--- src/apis/profile.ts
+++ src/apis/profile.ts
@@ -1,10 +1,10 @@
 import type { ProfileResponse } from "@/model/profile";
 import type { OperationResponse } from "@/model/resp";
-import { Get, Post } from "@/utils/request/auth_request";
+import { Auth_Get, Auth_Post } from "@/utils/request/request";
 
 // 获取用户信息
 function getProfile(): Promise<[any, ProfileResponse | undefined]> {
-    return Get("/profile/get");
+    return Auth_Get("/profile/get");
 }
 
 // 修改用户信息
@@ -15,7 +15,7 @@ function updateProfile(
     const form = new FormData();
     form.append("nickname", nickname);
     form.append("email", email);
-    return Post("/profile/update", form);
+    return Auth_Post("/profile/update", form);
 }
 
 
@@ -23,7 +23,7 @@ function updateProfile(
 function uploadAvatar(file: File): Promise<[any, OperationResponse | undefined]> {
     const form = new FormData();
     form.append("file", file);
-    return Post("/profile/avatar", form);
+    return Auth_Post("/profile/avatar", form);
 }
 
 
