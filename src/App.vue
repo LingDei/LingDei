71f6ecea6e81e5048b8266a767cf46fc854a9923
zjy2414 <zjy2414@outlook.com>
2023-11-04 21:17:18 +0800

    update: 暂时关闭静音提醒

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 import { RouterView } from 'vue-router'
 
 onMounted(() => {
-  ElMessage.info({
-    message: '本站视频开启了静音,如要听视频原声,请你手动取消静音',
-  })
+  // ElMessage.info({
+  //   message: '本站视频开启了静音,如要听视频原声,请你手动取消静音',
+  // })
 })
 </script>
 
