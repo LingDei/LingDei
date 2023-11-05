50dead9b99b90f1705f72908b5a0308fb767a635
zjy2414 <zjy2414@outlook.com>
2023-11-05 18:10:21 +0800

    update: 独立VideoBar

--- src/views/VideoView.vue
+++ src/views/VideoView.vue
@@ -7,6 +7,7 @@ import { handleNetworkError, handleRequestError } from '@/utils/request/RequestT
 import { Icon } from '@iconify/vue';
 import { useUserStore } from '@/stores/user';
 import { ElMessage as message } from 'element-plus'
+import VideoBar from '@/components/VideoBar.vue';
 
 const userStore = useUserStore()
 const route = useRoute()
@@ -59,83 +60,6 @@ async function checkCollect() {
     video_status.value.be_collected = data.status
 }
 
-// 点赞
-async function toggleLike() {
-    // 检查目前的点赞状态
-    if (video_status.value.be_liked) {
-        // 取消点赞
-        const [err, data] = await apis.deleteLike(video.value.uuid)
-        if (err) handleNetworkError(err)
-        if (data?.code != 200) {
-            handleRequestError(data)
-            return
-        }
-        video_status.value.be_liked = false
-    } else {
-        // 点赞
-        const [err, data] = await apis.addLike(video.value.uuid)
-        if (err) handleNetworkError(err)
-        if (data?.code != 200) {
-            handleRequestError(data)
-            return
-        }
-        video_status.value.be_liked = true
-        message({
-            showClose: false,
-            message: '点赞成功',
-            type: 'success'
-        })
-    }
-}
-
-// 收藏
-async function toggleCollect() {
-    // 检查目前的收藏状态
-    if (video_status.value.be_collected) {
-        // 取消收藏
-        const [err, data] = await apis.deleteCollect(video.value.uuid)
-        if (err) handleNetworkError(err)
-        if (data?.code != 200) {
-            handleRequestError(data)
-            return
-        }
-        video_status.value.be_collected = false
-    } else {
-        // 收藏
-        const [err, data] = await apis.addCollect(video.value.uuid)
-        if (err) handleNetworkError(err)
-        if (data?.code != 200) {
-            handleRequestError(data)
-            return
-        }
-        video_status.value.be_collected = true
-        message({
-            showClose: false,
-            message: '收藏成功',
-            type: 'success'
-        })
-    }
-}
-
-// 分享
-function shareVideo() {
-    // 弹出分享框，展示视频链接
-    const url = window.location.href
-    const title = video.value.name
-    const shareData = {
-        title: title,
-        url: url,
-    }
-
-    if (navigator.share) {
-        navigator.share(shareData)
-            .then(() => console.log('Successful share'))
-            .catch((error) => console.log('Error sharing', error));
-    } else {
-        console.log('navigator.share not supported')
-    }
-}
-
 </script>
 
 
@@ -156,24 +80,7 @@ function shareVideo() {
                 </div>
 
                 <!-- 操作按钮 -->
-                <div class="p-4 bg-white rounded-lg shadow-md">
-                    <div class="flex items-center space-x-4">
-                        <button class="flex items-center text-gray-500" @click="toggleLike">
-                            <Icon :icon="video_status.be_liked ? 'mdi:thumb-up' : 'mdi:thumb-up-outline'"
-                                :color="video_status.be_liked ? '#00aeec' : ''" class="w-6 h-6" />
-                            <span class="ml-2">点赞</span>
-                        </button>
-                        <button class="flex items-center text-gray-500" @click="toggleCollect">
-                            <Icon :icon="video_status.be_collected ? 'mdi:star' : 'mdi:star-outline'"
-                                :color="video_status.be_collected ? '#00aeec' : ''" class="w-6 h-6" />
-                            <span class="ml-2">收藏</span>
-                        </button>
-                        <button class="flex items-center text-gray-500" @click="shareVideo">
-                            <Icon icon="uil:share" class="w-6 h-6" />
-                            <span class="ml-2">分享</span>
-                        </button>
-                    </div>
-                </div>
+                <VideoBar v-if="Object.keys(video).length > 0" :video_uuid="video.uuid"></VideoBar>
 
                 <!-- 评论区 -->
                 <CommentCard v-if="Object.keys(video).length > 0" :video_uuid="video.uuid"></CommentCard>
@@ -181,13 +88,3 @@ function shareVideo() {
         </div>
     </div>
 </template>
-
-<style>
-.material-symbols-outlined {
-    font-variation-settings:
-        'FILL' 0,
-        'wght' 400,
-        'GRAD' 0,
-        'opsz' 24
-}
-</style>
